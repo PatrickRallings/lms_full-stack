eee46daf5f3b690dadc26e1b564061415bcaf92b
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-06-28 13:13:11 -0400

    Back-end initialization (spring initializer)

---
+++ lms_back-end/src/main/java/org/carolinafintechhub/lms_backend/LmsBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.carolinafintechhub.lms_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LmsBackEndApplication.class, args);
+    }
+
+}
---
+++ lms_back-end/src/test/java/org/carolinafintechhub/lms_backend/LmsBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.carolinafintechhub.lms_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
