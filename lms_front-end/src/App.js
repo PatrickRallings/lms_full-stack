0926885aecf717a868d1a7438010f3f1447bff7a
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-06-29 15:32:23 -0400

    Initial addUser component

--- lms_front-end/src/App.js
+++ lms_front-end/src/App.js
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar"
+import AddUser from "./components/AddUser";
 
 function App() {
   return (
     <>
       <Navbar />
+        <AddUser />
     </>
   );
 }
