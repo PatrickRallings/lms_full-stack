c3bb8220d9fb3a473af6b3e98c6a7c7e3f3963aa
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-06-29 16:25:47 -0400

    Created interface repository of roles that extends the JPARepository interface (which in turn extends CRUD)

---
+++ lms_back-end/src/main/java/org/carolinafintechhub/lms_backend/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package org.carolinafintechhub.lms_backend.repository;
+
+import org.carolinafintechhub.lms_backend.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+}
