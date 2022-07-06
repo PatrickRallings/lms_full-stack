1a0c6924ffb47b951356c8d978e32f4cd99b89ad
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-07-06 14:15:40 -0400

    Added basic (initial) methods to User service

--- lms_back-end/src/main/java/org/carolinafintechhub/lms_backend/services/UserService.java
+++ lms_back-end/src/main/java/org/carolinafintechhub/lms_backend/services/UserService.java
@@ -1,6 +1,13 @@
 package org.carolinafintechhub.lms_backend.services;
 
+import org.carolinafintechhub.lms_backend.model.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
+import java.util.List;
+
 public interface UserService extends UserDetailsService {
+
+    User getUserById(Long id);
+
+    List<User> getAllUsers();
 }
--- lms_back-end/src/main/java/org/carolinafintechhub/lms_backend/services/UserServiceImpl.java
+++ lms_back-end/src/main/java/org/carolinafintechhub/lms_backend/services/UserServiceImpl.java
@@ -1,9 +1,35 @@
 package org.carolinafintechhub.lms_backend.services;
 
+import org.carolinafintechhub.lms_backend.model.User;
+import org.carolinafintechhub.lms_backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class UserServiceImpl implements UserService{
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public User getUserById(Long id) {
+        Optional<User> optional = this.userRepository.findById(id);
+        if (optional.isPresent()) {
+            return optional.get();
+        } else {
+            throw new RuntimeException("User not found.");
+        }
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return this.userRepository.findAll();
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
